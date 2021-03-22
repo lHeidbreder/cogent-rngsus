21df3d320a3568e2b037d276b485c2f49ed5aefa
lHeidbreder <lukas@boeke.email>
2021-03-22 13:34:27 +0100

    Added setup scripts; should any more libraries be necessary in the future, make sure to update them

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo Setup will proceed to install the necessary libraries
+@pause
+
+py -3 -m pip install -U discord
+py -3 -m pip install -U requests
+
+@echo.
+@echo.
+@echo Please check the results
+@pause
